8ee3d7b2b7e2c2241ead8d59c550c5e933c59e5b
Admin <admin@charlie01-2.local>
2018-07-23 14:17:36 +0530

    subtract,multiply and divide added to post method

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -10,6 +10,23 @@ class HomepageController < ApplicationController
 
 	  if @operation.eql?"add"
 	  	@result = @x + @y
+	  
+	  elsif @operation.eql?"subtract"
+	  	@result = @x - @y
+	  
+	  elsif @operation.eql?"multiply"
+	  	@result = @x * @y
+
+	  elsif @operation.eql?"divide"
+	  	if @y == 0
+	  	  @result = "Undefined"
+	  	else
+	  	  @result = @x / @y
+	    end
+
+	  else 
+	  	@result= "Error ! no such operation exist"
+
 	  end
 
 	  render "index"
